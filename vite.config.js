2e70480cdc167ff423c8276da402c8154a097efa
Lavender0319 <101381680+Lavender0319@users.noreply.github.com>
2026-03-06 11:19:19 +1100

    Lexi_change_2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<travel->/',   // 这里改成你的仓库名，比如 '/myapp/'
+  base: '/travel-/',   // 这里改成你的仓库名，比如 '/myapp/'
 })
